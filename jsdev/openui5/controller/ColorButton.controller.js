9f585c6024a2f06ed1c6843e971f52dfe4300984
Alberto Lusiani <alusiani@gmail.com>
2020-07-04 18:07:38 +0200

    Nikola auto commit.
    Source commit: e0a0c5c167929c66182e3dd2bf3be6674d76ca85
    Nikola version: 8.1.0

---
+++ jsdev/openui5/controller/ColorButton.controller.js
@@ -0,0 +1,13 @@
+sap.ui.define([
+   'sap/ui/core/mvc/Controller',
+   'sap/m/Button',
+   'sap/ui/commons/ColorPicker'
+], function (Controller, Button, ColorPicker) {
+   "use strict";
+
+   return Button.extend("sap.ui.jsroot.controller.ColorButton", {
+
+
+   });
+
+});
